3077fded883a6a28108a7c0d7a2293340af5f854
Nathan Perkins <nathan@maxmo.net>
2014-05-30 20:02:54 -0400

    Improve documentation. Add main check to test script to prevent problem with Windows multiprocessing.

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 import numpy as np
-# from lintrain import Trainer
-from parallel.trainer import Trainer
+from lintrain import Trainer
 
-# generate random data
-num_entries = 50
-num_features = 10
-x = np.random.rand(num_entries, num_features)
-y = (30 * x[:, 0]) - (10 * x[:, 2]) + np.random.rand(1, num_entries)
+# must be placed outside the package and reference updated to use the parallel trainer
+# due to limitation related to relative references
+# from parallel.trainer import Trainer
 
-t = Trainer(x, y)
-t.debug = 2
-# t.run_forward_selection()
-# t.run_backward_selection()
-t.run_bidirectional_selection([1, 3])
+if '__main__' == __name__:
+    # generate random data
+    num_entries = 50
+    num_features = 10
+    x = np.random.rand(num_entries, num_features)
+    y = (30 * x[:, 0]) - (10 * x[:, 2]) + np.random.rand(1, num_entries)
 
-print "COLUMN COEFFICIENTS"
-print t.fit
+    # create trainer
+    t = Trainer(x, y)
+    t.debug = 2
 
-print "COLUMNS USED"
-print t.column_indices
+    # run
+    #t.run_forward_selection()
+    #t.run_backward_selection()
+    t.run_bidirectional_selection([1, 3])
+
+    # print output
+    print "COLUMN COEFFICIENTS"
+    print t.fit
+
+    print "COLUMNS USED"
+    print t.column_indices
