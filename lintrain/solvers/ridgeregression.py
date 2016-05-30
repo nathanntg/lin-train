1c1c7dd151b3b7894fff74b31c15bded4ac4dc96
Nathan Perkins <nathan@maxmo.net>
2016-05-30 12:54:28 -0400

    Allow setting ridge regression parameters during creation

--- lintrain/solvers/ridgeregression.py
+++ lintrain/solvers/ridgeregression.py
@@ -5,16 +5,21 @@ import numpy as np
 class RidgeRegression(Solver):
     """
     Analytically performs ridge regression, where coefficients are regularized by learning rate alpha. This constrains
-    coefficients and can be effective in situations where over- or under-fitting arise.
+    coefficients and can be effective in situations where over- or under-fitting arise. Parameters `alpha`
+    is the regularization constant (alpha of 0 is least squares, as alpha increases, coefficients are increasingly
+    constrained).  Parameter `intercept` (defaults to true) causes an intercept column (all ones) to automatically be
+    detected and excluded from regularization.
+
     Based off of:
     https://gist.github.com/diogojc/1519756
     """
 
-    alpha = 0.1
-    intercept = True
-
-    def __init__(self):
+    def __init__(self, alpha=0.1, intercept=True):
         Solver.__init__(self)
+
+        # parameters
+        self.alpha = alpha # regularization constant
+        self.intercept = intercept # automatically gues intercept and do not regularize
 
     def calculate_parameters(self, x, y):
         g = self.alpha * np.eye(x.shape[1])
